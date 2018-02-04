0a51b79566716ef695814d9c13a135ad800c76c2
ivandoroshenko <ivandoroshenko1988@gmail.com>
2018-02-04 16:00:46 +0200

    finish main work

--- src/js/main.js
+++ src/js/main.js
@@ -16,17 +16,7 @@ $(function() {
         }
 
     });
-        //navigation click======
-        $('.nav__item a').click(function (e) {
-            e.preventDefault();
-            console.log($(this).attr('href'));
-            var getHref = $(this).attr('href');
-            var jump = $(getHref).offset().top;
-            $('html, body').animate({
-                scrollTop: jump
-            }, 1000);
-        });
-
+      
     $('.intro__slider').slick({
         dots: false,
         arrows: true,
@@ -43,21 +33,6 @@ $(function() {
     });
    
 
-    //button up
-    $(document).click(function (event) {
-        btnUp = event.target;
-        if ($(btnUp.parentNode).hasClass('btn-up') ) {
-            $('body, html').animate({ 'scrollTop': 0 }, 1000);
-        }
-    });
-    $(document).scroll(function (e) { 
-        e.preventDefault;
-        if ($(document).scrollTop() > 200) {
-            $('.btn-up').addClass('btn-up--shown');
-        } else {
-            $('.btn-up').removeClass('btn-up--shown')
-       }
-    }); 
     
     
 }); 
